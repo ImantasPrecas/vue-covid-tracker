630744be90c15a73e0751c8d80f560277ef0b320
Imantas <precasimantas@gmail.com>
2022-09-16 14:54:44 +0300

    #

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="text-center bg-blue-700 text-white py-4 mb-10">
+    <h1 class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </h1>
+    <p>
+      API by
+      <a class="text-blue-300" href="https://covid19api.com" target="_blank"
+        >Covid19api.com</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching data</div>
+    <img class="w-10 m-auto" :src="loadingImg" alt="" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataBoxes from "../components/DataBoxes.vue";
+import DataTitle from "../components/DataTitle.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: { DataTitle, DataBoxes },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: "",
+      countries: [],
+      loadingImg: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const rez = await await fetch("https://api.covid19api.com/summary");
+      const data = await rez.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.countris = data.Countries;
+    this.stats = data.gloabal;
+
+    this.loading = false;
+
+    console.log(data);
+  },
+};
 </script>
